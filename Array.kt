52dbd750b994d2e8b20103f19f0e1ab2604876c9
=ridwanrizkia04@gmail.com <ridwanrizkia04@gmail.com>
2023-12-30 15:02:08 +0700

    Keyword

---
+++ Array.kt
@@ -0,0 +1,37 @@
+fun main(){
+    var mobile = arrayOf("Avanza","Fortuner","Alphad")
+    println(mobile[0])
+
+    /*
+    Cara lain
+     */
+    var buah = arrayOf("apel","nanas","melon")
+    buah += "durian"
+    println(buah.joinToString())//joinToString()Fungsi ini berguna untuk menggabungkan elemen-elemen dari suatu koleksi ke dalam satu string dengan pemisah tertentu.
+
+    /*
+    dan perlu di ketahui bawah array mempunyai beberapa macam fungsi di antranya:
+     arrayOf(), arrayOfNulls()atau emptyArray().*/
+    /*
+     arrayOf():
+    Fungsi ini digunakan untuk membuat array dengan elemen yang sudah ditentukan.*/
+    var nomor = arrayOf(1,2,3)
+    println(nomor.joinToString())
+
+    /*
+    arrayOfNulls():
+    Fungsi ini digunakan untuk membuat array dengan elemen-elemen null dan ukuran tertentu.
+     */
+    val nularay :Array<Int?> = arrayOfNulls(5)
+    println(nularay.joinToString())
+    /*
+    emptyArray():
+    Fungsi ini digunakan untuk membuat array kosong dengan tipe elemen tertentu.
+     */
+    val emptyStringArray = emptyArray<String>()
+    println(emptyStringArray)
+
+
+
+
+}
---
+++ Break &Continu.kt
@@ -0,0 +1,22 @@
+fun main(){
+    var i = 0
+    while (i < 10){
+        i++
+        println("$i Mengulang''''")
+
+        if (i == 5){
+            break
+        }
+
+    }
+    var x = 0
+    while (true){
+        println("Progres mengulang $x ")
+        x++
+        if (x > 1000){
+            break
+        }
+    }
+
+
+}
